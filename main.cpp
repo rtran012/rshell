93f4e1a9a5fcd7f338f5066c63c590cc322826c9
Richard <rtran012@ucr.edu>
2014-10-18 16:05:48 -0700

    cleaning up code

--- main.cpp
+++ main.cpp
@@ -43,36 +43,6 @@ int parse(char userinput[], char **argv){
 
 	argv[numargs] = strtok(userinput," \t\n");
 		
-  /*  	while(argv[numargs] != NULL){
-        	if(!strcmp(argv[numargs],"#")){
-            		argv[numargs] = NULL;
-            		if(numargs > 0) numargs--;
-            		break;
-        	}   
-		char *copy = argv[numargs];
-		int length = strlen(argv[numargs]);
-		if(copy[0] == '#'){
-			argv[numargs] = NULL;
-			if(numargs > 0) numargs--;
-			break;
-		}
-		if(copy[length-1] == '#'){
-			copy[length-1] = '\0';
-			break;
-		}
-		bool shouldbreak=false;
-		for(int i=0; i<length; i++) {
-			if(copy[i] == '#'){
-				copy[i] = '\0';
-				argv[numargs] =copy;
-				shouldbreak=true;
-			}
-		}
-		if(shouldbreak) break;
-
-        	numargs++;
-		argv[numargs] = strtok(NULL, " \t\n");
-    	}*/
 	while(argv[numargs] != NULL){
 		if(strcmp(argv[numargs], ";")==0){
 			argv[numargs] = NULL ;
@@ -83,11 +53,6 @@ int parse(char userinput[], char **argv){
 
 		numargs++;
 		argv[numargs] = strtok(NULL," \t\n");
-//		if(strcmp(argv[numargs], ";")==0){
-//			excute(argv);
-//			numargs = 0;
-//			argv[numargs]=strtok(NULL," \t\n");
-//		}
 	}
 	excute(argv);
 	cout << "arguments " << endl;
@@ -96,44 +61,6 @@ int parse(char userinput[], char **argv){
 	}
     	return numargs;
 }
-/*
-bool background(int numargs, char** argv, bool emptyinput){
-	bool backgroundProc;
-	if(numargs > 0) numargs --;
-	if(!emptyinput && !strcmp(argv[numargs],"&")){
-        	backgroundProc = true;
-        	argv[numargs] = NULL;
-    	}
-	else if(!emptyinput){
-        	char* copy = argv[numargs];
-        	int arglen = strlen(argv[numargs]) - 1;
-        	if(copy[arglen] == '&'){
-            		backgroundProc=true;
-			copy[arglen] = '\0';
-            		argv[numargs] = copy;
-        	}
-    	}
-    	return backgroundProc;
-}
-
-void excute(char** argv){
-	int pid = fork();
-	if(pid == -1){
-		perror("there was an error with fork().");
-	}
-	else if (pid == 0){
-		if(execvp(argv[0], argv) == -1){
-			perror("there was an error with execvp.");
-		}
-		exit(1);
-	}
-	else if (pid > 0){
-		if(-1 == wait(0)){
-			perror("there was an error with wait().");
-		}
-	}
-}
-*/
 int main(){
 
 	while(1){
@@ -150,30 +77,15 @@ int main(){
 		*/char ** argv;
 		argv = new char *[50];
 
-//		bool emptyInput = false;
-//		if(!strcmp(input, "")) emptyInput = true;
 
 			parse(input, argv);
-		/*
-		int j = 0;
-	//	int loc=0;
 		for(j =0; j < i; j++){
 			if(strcmp(argv[j],";")) {
-	//			char ** new_argv;
-	//			new_argv = new char *[50];
-	//			for(int k = 0 ; k < j; k++){
-	//				new_argv[k] = argv[j];
-	//			}
-				 
 				excute(argv);
 				delete[] new_argv;
-	//			loc = j+1;
 			}
 		}	
 		
-//		bool backgroundProc = false;
-//		backgroundProc = background (i, argv, emptyInput);
-		//excute(argv);		*/
 		delete[] argv;
 	}
 	return 0;
