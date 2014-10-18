3913c6c07f352bf66cd34708613915a1bdf6733d
Richard <rtran012@ucr.edu>
2014-10-18 02:49:24 -0700

    new changes

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <unistd.h>
+#include <string.h>
+#include <iostream>
+#include <string>
+#include <stdlib.h>
 
-int main(){
-	return 0;
+using namespace std;
+
+int main(int argc, char **argv){
+	cout << "hi" << endl;	
+	int fail = execvp(argv[0], argv);
+	cout << "blab" << endl;	
+	if (fail == -1){
+		cout << "should not happen" << endl;
+		return 1;
+	}
+	 return 0;
+
+
 }
