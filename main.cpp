0d46fd52c1a6a1bcd38b0850ad7a1c0f0f3dbddb
Richard <rtran012@ucr.edu>
2014-10-19 21:15:40 -0700

    hw0 working

--- main.cpp
+++ main.cpp
@@ -46,15 +46,15 @@ int parse(char userinput[], char **argv){
 	}
 
 	argv[numargs] = strtok(userinput," \t\n");
-		
+
 	while(argv[numargs] != NULL){
-		if(strcmp(argv[numargs], ";")==0){
-			argv[numargs] = NULL ;
+		if(strcmp(argv[numargs], ";") == 0){
+			argv[numargs] = NULL;
 			stat = excute(argv);
-			cout << "stat " << stat << endl;
-                         numargs = 0;
-                         argv[numargs]=strtok(NULL," \t\n");
-                 }
+			numargs = 0;
+			argv[numargs] = strtok(NULL, " \t\n");
+		}
+
 		if(strcmp(argv[numargs], "&&") == 0){
 			argv[numargs] = NULL;
 			stat = excute(argv);
@@ -62,6 +62,7 @@ int parse(char userinput[], char **argv){
 			numargs = 0;
 			argv[numargs] = strtok(NULL, " \t\n");
 		}
+
 		if(strcmp(argv[numargs], "||") == 0){
 			argv[numargs] = NULL;
 			stat = excute(argv);
@@ -73,14 +74,31 @@ int parse(char userinput[], char **argv){
 		argv[numargs] = strtok(NULL," \t\n");
 	}
 	stat = excute(argv);
-//	cout << "stat " << stat << endl;
-    	return numargs;
+    	return stat;
 }
 int main(){
+	char username[100] = {0};
+	char hostname[100] = {0};
+
+	if(getlogin_r(username, sizeof(username)-1)){
+		perror("There was an error in getlogin_r. ");
+	}
+	if(-1 == gethostname(hostname, sizeof(hostname)-1)){
+		perror("There was an erro in gethostname. ");
+	}
+	for(int i = 0; i < 30 ; i++){
+		if(hostname[i] == '.'){
+			hostname[i] = '\0';
+		}
+	}
 
 	while(1){
-		cout << "$ ";
+		cout << username << "@" << hostname << "$ ";
 		char input[1024];
+		char buff[1024];
+		char * first, * next;
+		int stat;
+		bool stat1;
 		cin.getline(input,1024);
 		
 		if(!strcmp(input, "exit")) exit(1);
@@ -89,8 +107,59 @@ int main(){
 		
 		argv = new char *[50];
 
+		if(strstr(input,";") != NULL){
+			first = input;
+			next = strstr(first,";");
+			while(next != NULL){
+				for(int i =0; i < 1024; i++) buff[i]=0;
+				strncpy(buff,first,next-first);	
+				stat = parse(buff, argv);
+				first = next + 1;
+				next = strstr(first,";");
+			}
 
-		parse(input, argv);
+			for(int i =0; i < 1024; i++) buff[i]=0;	
+			strcpy(buff, first);
+			stat = parse(buff, argv);
+		}
+
+		else if(strstr(input,"&&") != NULL){
+			first = input;
+			next = strstr(first,"&&");
+			while(next != NULL){
+				for(int i =0; i < 1024; i++) buff[i]=0;
+				strncpy(buff,first,next-first);	
+				stat = parse(buff, argv);
+				if(stat != 0) break;
+				first = next+2;
+				next = strstr(first,"&&");
+			}
+
+			for(int i =0; i < 1024; i++) buff[i]=0;	
+			strcpy(buff, first);
+			stat = parse(buff, argv);
+		}
+		else if(strstr(input,"||") != NULL){
+			first = input;
+			stat1 = true;
+			next = strstr(first,"||");
+			while(next != NULL && stat1){
+				for(int i =0; i < 1024; i++) buff[i]=0;
+				strncpy(buff,first,next-first);	
+				stat = parse(buff, argv);
+				if(stat == 0) stat1 = false;
+				first = next+2;
+				next = strstr(first,"||");
+			}
+			if(stat1){
+				for(int i =0; i < 1024; i++) buff[i]=0;	
+				strcpy(buff, first);
+				stat = parse(buff, argv);
+			}
+		}
+		else{
+			stat = parse(input,argv);
+		}
 		
 		delete[] argv;
 	}
