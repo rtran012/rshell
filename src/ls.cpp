32486fb254b476cfe81fb8e850a4f3b7703d0a37
richard <rtran012@ucr.edu>
2014-10-30 00:31:59 -0700

    simple outline

--- src/ls.cpp
+++ src/ls.cpp
@@ -1,1 +1,24 @@
-test 
+#include <iostream>
+using namespace std;
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#define FLAG_a 1
+#define FLAG_l 2
+#define FLAG_r 3
+#define FLAG_R 4
+
+int main(int argc, char**argv)
+{
+	struct stat statbuf;
+	stat(argc, &
+
+/* find what flags i need
+	then output the desired input
+	in the desired out put create struct and then output desired information
+*/
+
+
+	return 0;
+}
