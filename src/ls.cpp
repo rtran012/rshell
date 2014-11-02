27403f5d375dbe4f83bc3202c454e6c9e1d96490
richard <rtran012@ucr.edu>
2014-11-02 11:31:54 -0800

    got ls and ls -a to work with dir

--- src/ls.cpp
+++ src/ls.cpp
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <vector>
 #include <string>
+#include <sys/types.h>
+#include <dirent.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -14,6 +16,32 @@ using namespace std;
 int aflag = 0;
 int lflag = 0;
 int Rflag = 0;
+
+void output_ls(char * dirName){
+	
+	DIR *currPath;
+	struct dirent * currFile;
+	
+	currPath = opendir(dirName);
+	if(currPath == NULL){
+			perror("opendir: ");
+		
+	}
+	else{
+		while((currFile = readdir(currPath)) != NULL){
+			if(currFile->d_name[0] != '.'){
+				cout << "currFile " << currFile->d_name << endl;
+			}
+			else if (aflag == 1 && currFile->d_name[0] == '.'){
+				cout << "currFile " << currFile->d_name << endl;
+			}
+
+		}
+		closedir(currPath);
+	}
+
+	
+}
 
 int main(int argc, char**argv)
 {
@@ -44,5 +72,23 @@ int main(int argc, char**argv)
 	cout << "R =" << Rflag << endl;
 	for(index = optind ; index < argc; index++)
 		cout<< index << " Non-option argument " << argv[index] << endl ;	
+	//assume at least one argument
+	if(optind == argc) {
+		char dir_p[2];
+		strcpy(dir_p, ".");
+		output_ls(dir_p);
+	}
+	else {
+		for(index = optind ; index < argc; index++)
+			output_ls(argv[index]);
+	}
 	return 0;
 }
+
+
+
+
+
+
+
+
