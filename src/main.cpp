5bbfb248d79ee21f85df5f49fb38d204df293738
richard <rtran012@ucr.edu>
2014-11-12 21:26:31 -0800

    working piping and redirection

--- src/main.cpp
+++ src/main.cpp
@@ -8,30 +8,9 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+#include <fcntl.h>
 
 using namespace std;
-
-int excute(char** argv){
-	
-	int status;
-        int pid = fork();
-        
-	if(pid == -1){
-                 perror("there was an error with fork().");
-         }
-         else if (pid == 0){
-                 if(execvp(argv[0], argv) == -1){
-                         perror("there was an error with execvp.");
-                 }
-                 exit(errno);
-         }
-        
-	 else if (pid > 0){
-		wait(&status);
-		return status;
-         }
-	return status;
-} 
 
 int gen_cmd( char userinput[], char **argv){
         int numargs=0;
@@ -44,21 +23,96 @@ int gen_cmd( char userinput[], char **argv){
         return numargs; 
 }
 
-int parse2(char userinput [], char **argv){
+/* This routine will execute the command with redirection option */
+int scan_cmd_redirect( char userinput[], char **argv) {
+//        int numargs = 0;
+        char *filein, *fileout;
+        char *curr_pt_in, *curr_pt_out;
+        char temp_in[1024];
+        char temp_out[1024];
+
+        //cout << " Input: userinput = " << userinput << endl;
+
+        // Search for input redirection  information
+        filein = NULL; 
+        if ( (curr_pt_in = strstr(userinput,"<")) != NULL ) {
+             strcpy(temp_in, curr_pt_in+1);
+             filein = strtok(temp_in," \n\t><");
+             if ( filein == NULL ) {
+//                  numargs = -1;
+                  cout << "Can not find <  in userinput " << userinput << endl;
+            }
+        } 
+
+        // Search for output redirection information
+        fileout = NULL;
+        if ( (curr_pt_out = strstr(userinput,">")) != NULL){
+            strcpy(temp_out, curr_pt_out+1);
+            fileout = strtok(temp_out," \n\t><");
+            if (fileout == NULL) {
+    //             numargs = -1;
+                 cout << "Can not fine > in userinput " << userinput << endl;
+            }
+        }
+
+
+        // mark end of the current command
+        if ( curr_pt_in  != NULL ) *curr_pt_in  = 0;
+        if ( curr_pt_out != NULL ) *curr_pt_out = 0;
+
+        //if (filein  != NULL ) cout <<  " Redirect input: " << filein << endl;
+        //if (fileout != NULL ) cout <<  " Redirect output: " << fileout << endl;
+
+
+//        int status;
+        int new_stdin, new_stdout;
+
+        new_stdin = -1; 
+        new_stdout = -1;
+
+        // Open both input/output redirection if exists
+        if ( filein != NULL ) 
+                new_stdin = open (filein, O_RDONLY);
+        if ( fileout != NULL )
+                new_stdout = open(fileout, O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU|S_IRWXG|S_IRWXO);
+
+        // Replacing input redirection
+        if ( new_stdin != -1 ) {
+                close ( 0 );
+                dup( new_stdin );
+                close ( new_stdin );
+        }
+
+        // Replacing output redirection
+        if ( new_stdout != -1 ) {
+                close ( 1 );
+                dup ( new_stdout );
+                close ( new_stdout );
+        }
+
+        int nargs;
+        //int i;
+        nargs = gen_cmd(userinput, argv);
+
+       return nargs;      
+}
+
+int parse(char userinput [], char **argv){
         //int  istat;
 	char ** cmd;
 	char * first;
 	int ncmd = 0;
         int icmd;
 
-	cout << "userinput " << userinput << endl;	
+//	cout << "userinput " << userinput << endl;	
 
-        char op_cmd[50];
+        //char op_cmd[50];
 
 	cmd = new char *[50];
 
         //cout << "DEBUG userinput = " << userinput << endl;
         //cout << " strlen = " << strlen(userinput) << endl;
+
         first = userinput;
         icmd  = 0;
 	int nlen = strlen(userinput);
@@ -66,108 +120,79 @@ int parse2(char userinput [], char **argv){
             if(strncmp (&userinput[i], "|", 1 ) == 0 ) {
                  cmd[icmd] = first;
                  first = &userinput[i+1];
-                 op_cmd[icmd] = '|';
-                 userinput[i] = 0;
-                 icmd++;
-            } else if ( strncmp( &userinput[i], "<", 1 ) == 0 ) {
-                 cmd[icmd] = first;
-                 first = &userinput[i+1];
-                 op_cmd[icmd] = '<';
-                 userinput[i] = 0;
-                 icmd++;
-            } else if ( strncmp( &userinput[i], ">", 1 ) == 0 ) {
-                 cmd[icmd] = first;
-                 first = &userinput[i+1];
-                 op_cmd[icmd] = '>';
+        //         op_cmd[icmd] = '|';
                  userinput[i] = 0;
                  icmd++;
             }
         }
         cmd[icmd] = first;
-        op_cmd[icmd] = 0;
+     //   op_cmd[icmd] = 0;
 	ncmd = icmd;
 	
 
-        cout << "Found there are " << icmd << " operator "<<endl;
-        for (int i=0; i < icmd ; i++){
-           cout << i << " cmd= "<< cmd[i] << endl;
-           cout << "   the operator is " << op_cmd[i] << endl;
-        }
-        cout << icmd  << " cmd= "<< cmd[ icmd] << endl;
+        //cout << "Found there are " << ncmd << " operator "<<endl;
+        //for (int i=0; i < ncmd ; i++){
+        //   cout << i << " cmd= "<< cmd[i] << endl;
+        //   cout << "   the operator is " << op_cmd[i] << endl;
+        //}
+        //cout << icmd  << " cmd= "<< cmd[ ncmd] << endl;
 	
 
     //    if (icmd > 0 ) return ncmd;
 
-/*	first = userinput;
-	next = strstr(first, "|");
-        if ( next == NULL ) {
-
-           gen_cmd( userinput, argv);
-           istat = excute(argv);
-
-        } else {
-
-	cmd[0] = first;
-	ncmd++;
-	while(next != NULL){
-		cmd[ncmd] = next+1;
-		*next = 0 ;
-		first = next +1;
-		next = strstr(first, "|");	
-		ncmd++;
-	}	
-
-	//cout << "ncmd " << ncmd << endl;
-	//for(int i = 0; i < ncmd; i++){
-        //    cout<< i << " cmd[i] " << cmd [i] << endl;
-        //}
-*/
 
         /* implement the piping function */
-        //int  nargs;
-//        int  pid, status;
 	int pid;
 	int status;
 	int newfd[2];
-//        int  icur_pipe;
         int  fd[2];
 	if(ncmd == 0){
-		gen_cmd(cmd[0], argv);
-		status = excute(argv);
-		return status;
+
+                pid = fork ();
+                if ( pid == -1 ) {
+                      perror("There was an error with fork().");
+                } else if ( pid == 0 ) {
+                      scan_cmd_redirect ( cmd[0], argv );
+                      if ( execvp ( argv[0], argv ) == -1 ) 
+                                  perror ("there was an error with execvp." );
+                      exit(errno);
+                } else if ( pid > 0 ) {
+                      wait( &status );
+                      return status;
+                }
+                return status;
+                       
 	}
 	else{	//taking care of piping
 		pipe(fd);
 		switch( pid = fork()){
 			case 0: 
-				if(strncmp(& op_cmd[0], "|", 1) == 0){
-					dup2( fd[1], 1);
-					close( fd[0] );
-				}
-				gen_cmd(cmd[0], argv);
-				if( execvp( argv[0], argv) == -1) {
-					perror("execvp " );
-				}
-				exit(errno);	
+                                scan_cmd_redirect ( cmd[0], argv );
+
+				dup2( fd[1], 1);
+				close( fd[0] );
+
+                                if ( execvp ( argv[0], argv ) == -1 )
+                                      perror ( "execvp ");
+                                exit ( errno );
 			case -1:
 				perror("fork ");
 				exit(1);
 		}	
-		for(int j= 1; j < ncmd - 1; j++){
+		for(int j= 1; j < ncmd ; j++){
 			pipe(newfd);
 			switch( pid = fork()){
 				case 0:
-					if(strncmp( &op_cmd[j], "|", 1) == 0){
-						dup2( fd[0], 0);
-						close( fd[1] );
-						dup2( newfd[1], 1);
-						close( newfd[0]); 
-					}
-					gen_cmd(cmd[j], argv);
-					if( execvp( argv[0], argv) == -1){
-						perror("execvp " );
-					}
-					exit(errno);
+                                        scan_cmd_redirect ( cmd[j], argv );
+
+					dup2( fd[0], 0);
+					close( fd[1] );
+					dup2( newfd[1], 1);
+					close( newfd[0]); 
+
+                                        if ( execvp ( argv[0], argv ) == -1 )
+                                              perror ( "exevp ");
+                                        exit ( errno );
 				case -1:
 					perror("fork ");
 					exit(1);
@@ -179,13 +204,14 @@ int parse2(char userinput [], char **argv){
 		}
 		switch( pid = fork()) {
 			case 0:
+                                scan_cmd_redirect ( cmd[ncmd], argv );
+
 				dup2( fd[0], 0);
 				close( fd[1] );
-				gen_cmd(cmd[ncmd], argv);
-				if( execvp(argv[0], argv) == -1){
-					perror("execvp ");
-				}
-				exit(errno);
+
+                                if ( execvp ( argv[0], argv ) == -1 )
+                                      perror ( "exevp ");
+                                exit ( errno );
 			case -1:
 				perror("fork ");
 				exit(1);
@@ -194,45 +220,10 @@ int parse2(char userinput [], char **argv){
 		close( fd[1] );
 		while((pid = wait(&status)) != -1);
 	}
-	if(ncmd == 0)
-		return ncmd;
-	return ncmd;
+
+	return status;
 }
 
-int parse(char userinput[], char **argv){
-	int stat;   
-	int numargs=0;
-	argv[numargs] = strtok(userinput," \t\n");
-
-	while(argv[numargs] != NULL){
-/*		if(strcmp(argv[numargs], ";") == 0){
-			argv[numargs] = NULL;
-			stat = excute(argv);
-			numargs = 0;
-			argv[numargs] = strtok(NULL, " \t\n");
-		}
-
-		if(strcmp(argv[numargs], "&&") == 0){
-			argv[numargs] = NULL;
-			stat = excute(argv);
-			if(stat != 0) return numargs;
-			numargs = 0;
-			argv[numargs] = strtok(NULL, " \t\n");
-		}
-
-		if(strcmp(argv[numargs], "||") == 0){
-			argv[numargs] = NULL;
-			stat = excute(argv);
-			if(stat == 0) return numargs;
-			numargs = 0;
-			argv[numargs] = strtok(NULL, " \t\n");
-		}*/
-		numargs++;
-		argv[numargs] = strtok(NULL," \t\n");
-	}
-	stat = excute(argv);
-    	return stat;
-}
 int main(){
 	char username[100] = {0};
 	char hostname[100] = {0};
@@ -335,7 +326,7 @@ int main(){
 		
 		// single argument 
 		else{
-			stat = parse2(input,argv);
+			stat = parse(input,argv);
 		}
 		
 		delete[] argv;
